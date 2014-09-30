23819772cd43991454ce1c845286bf2090f6871b
Marcel Mueller <com2040@gmail.com>
2014-09-30 19:30:17 +0200

    This added the header definitions for monitors

--- src/compositor/module.h
+++ src/compositor/module.h
@@ -28,6 +28,37 @@
 #ifndef __WS_COMPOSITOR_MODULE_H__
 #define __WS_COMPOSITOR_MODULE_H__
 
+#include <stdint.h>
+#include <stdlib.h>
+#include <xf86drmMode.h>
+/**
+ * A framebuffer device
+ */
+struct ws_framebuffer_device {
+    int fd; //<! The FileDescriptor
+    const char* path; //<! The path to the given filedescriptor
+};
+
+/**
+ * A connector, which under DRM translates to a Device+Output
+ */
+struct ws_monitor {
+    struct ws_monitor* next;
+    int connected;
+    uint32_t width;
+    uint32_t height;
+    uint32_t stride;
+
+    uint32_t size;
+    uint8_t* map;
+
+    drmModeModeInfo mode;
+    uint32_t fb;
+    uint32_t conn;
+    uint32_t crtc;
+    drmModeCrtc* saved_crtc;
+};
+
 /**
  * Initialise the compositor
  *
@@ -35,7 +66,7 @@
  * This includes allocating resources and performing initial discovery.
  * The function takes care of initialising the compositor only once, it is save
  * to call this function multiple times.
- * 
+ *
  *
  * @return 0 if the initialisation was successful, a negative error code on
  *         failure
