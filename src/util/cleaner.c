7adc563bb943c36ba75cc3c86a29ee7ad4bcbbe9
Matthias Beyer <mail@beyermatthias.de>
2014-09-30 18:21:14 +0200

    Add cleaner implementation

--- src/util/cleaner.c
+++ src/util/cleaner.c
@@ -25,4 +25,119 @@
  * along with waysome. If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <errno.h>
+#include <pthread.h>
+#include <stdlib.h>
+
 #include "util/cleaner.h"
+
+/**
+ * Cleaner linked list entry
+ */
+struct cleaner_entry {
+    struct cleaner_entry* next; //!< Next cleaner entry
+    void (*func)(void*); //!< Function to call
+    void* etc; //!< Argument for the function
+};
+
+/**
+ * Cleaner singleton
+ *
+ * Insertion should be at beginning, calling the linked list from the first
+ * entry to the last. So the first inserted entry can be the cleaner for the
+ * cleaner itself.
+ */
+static struct cleaner {
+    struct cleaner_entry* head; //!< Head of linked list for clean functions
+    pthread_mutex_t mtx; //!< Mutex for cleanup linked list
+} cleaner;
+
+/*
+ *
+ *
+ * static functions
+ *
+ *
+ */
+
+/**
+ *
+ *
+ */
+static void
+cleanup_entry(
+    struct cleaner_entry* head
+) {
+    if (head->next) {
+        cleanup_entry(head->next);
+    }
+
+    free(head);
+}
+
+/**
+ * Cleanup function for cleaner itself
+ */
+static void
+cleanup(
+    void* cleaner //!< Cleanup argument
+) {
+    cleanup_entry(((struct cleaner*) cleaner)->head);
+    pthread_mutex_destroy(&((struct cleaner*) cleaner)->mtx);
+}
+
+/*
+ *
+ * Interface implementation
+ *
+ */
+
+void
+ws_cleaner_init(void)
+{
+    cleaner.head = NULL;
+    pthread_mutex_init(&cleaner.mtx, NULL);
+    ws_cleaner_add(cleanup, &cleaner);
+}
+
+int
+ws_cleaner_add(
+    void (*cleaner_func)(void*),
+    void* etc
+) {
+    if (!etc) {
+        return -EINVAL;
+    }
+
+    struct cleaner_entry* e = calloc(1, sizeof(*e));
+
+    if (!e) {
+        return -ENOMEM;
+    }
+
+    e->etc = etc;
+    e->func = cleaner_func;
+
+    pthread_mutex_lock(&cleaner.mtx);
+    e->next = cleaner.head;
+    cleaner.head = e;
+    pthread_mutex_unlock(&cleaner.mtx);
+
+    return 0;
+}
+
+bool
+ws_cleaner_run(void)
+{
+    pthread_mutex_lock(&cleaner.mtx);
+    struct cleaner_entry* iter = cleaner.head;
+    struct cleaner_entry* next;
+
+    while (iter) {
+        next = iter->next;
+        iter->func(iter->etc);
+        iter = next;
+    }
+    return true;
+}
+
