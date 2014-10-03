cbdb02f4ba6f34b43f8a36156aa5fd56fd5c58f7
Matthias Beyer <mail@beyermatthias.de>
2014-10-03 18:38:14 +0200

    Add likely()/unlikely() macros

--- src/util/condition.h
+++ src/util/condition.h
@@ -40,6 +40,18 @@
 #ifndef __WS_UTIL_CONDITION_H__
 #define __WS_UTIL_CONDITION_H__
 
+#ifdef __GNUC__
+
+#define likely(x)   __builtin_expect(!!(x),1)
+#define unlikely(x) __builtin_expect(!!(x),0)
+
+#else
+
+#define likely(x)   (x)
+#define unlikely(x) (x)
+
+#endif //__GNUC__
+
 
 
 #endif // __WS_UTIL_CONDITION_H__
