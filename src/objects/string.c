367a3b69570a259a678bca880fb0471045751fb7
Nadja Sommerfeld <nadja.sommerfeld@hs-furtwangen.de>
2014-10-07 21:43:36 +0200

    Add ws_string method stubs

--- src/objects/string.c
+++ src/objects/string.c
@@ -25,10 +25,147 @@
  * along with waysome. If not, see <http://www.gnu.org/licenses/>.
  */
 
-
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unicode/ustring.h>
 
 #include "objects/string.h"
+
+#include "objects/object.h"
+#include "objects/array.h"
+
+/*
+ *
+ *
+ * forward declarations  
+ *
+ *
+ */
+
+static bool
+deinit_callback(
+    struct ws_object* const
+);
+
+/*
+ *
+ *
+ * v-table
+ *
+ *
+ */
+
+ws_object_type_id WS_OBJECT_TYPE_ID_STRING = {
+    .supertype = &WS_OBJECT_TYPE_ID_OBJECT,
+    .typestr = "ws_string",
+    
+    .init_callback = NULL,
+    .deinit_callback = deinit_callback,
+    .log_callback = NULL,
+    .run_callback = NULL,
+    .hash_callback = NULL,
+    .cmp_callback = (int (*) (struct ws_object const*, struct ws_object const*))
+                    ws_string_cmp,
+};
+
+bool
+ws_string_init(
+    struct ws_string* self
+) { 
+    //!< @todo implement
+    return false; 
+}
+
+struct ws_string*
+ws_string_new(void)
+{
+    //!< @todo implement
+    return NULL;
+}
+
+size_t
+ws_string_len(
+    struct ws_string* self
+){
+    //!< @todo implement
+    return 0;
+}
+
+struct ws_string*
+ws_string_cat(
+    struct ws_string* self,
+    struct ws_string* other
+){
+    //!< @todo implement
+    return NULL;
+}
+
+
+struct ws_string*
+ws_string_multicat(
+    struct ws_string* self,
+    struct ws_array* others
+){
+    //!< @todo implement
+    return NULL; 
+ }
+ 
+
+struct ws_string*
+ws_string_dupl(
+    struct ws_string* self
+){
+    //!< @todo implement
+    return NULL;
+}
+
+int
+ws_string_cmp(
+    struct ws_string* self,
+    struct ws_string* other
+){
+    //!< @todo implement
+    return 0;
+}
+
+int
+ws_string_ncmp(
+    struct ws_string* self,
+    struct ws_string* other,
+    size_t offset,
+    size_t n
+){
+    //!< @todo implement
+    return 0;
+}
+
+bool
+ws_string_substr(
+    struct ws_string* self,
+    struct ws_string* other
+){
+    //!< @todo implement
+    return false;
+}
+
+char*
+ws_string_raw(
+    struct ws_string* self
+){
+    //!< @todo implement
+    return NULL;   
+}
+
+/*
+ *
+ * Static Function Implementations
+ *
+ */
+static bool
+deinit_callback(
+    struct ws_object* const self
+) {
+    //!< @todo implement
+    return true;
+}
