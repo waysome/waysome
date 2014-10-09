8ba6f1f8cf1755ad359c73f1b20adc55a9a2880a
Julian Ganz <Julian.Ganz@hs-furtwangen.de>
2014-10-09 18:22:46 +0200

    Add preliminary compositor interface (stubs)

--- src/compositor/wayland_compositor.c
+++ src/compositor/wayland_compositor.c
@@ -25,5 +25,75 @@
  * along with waysome. If not, see <http://www.gnu.org/licenses/>.
  */
 
+// wayland-server.h has to be included before wayland-server-protocol.h
+#include <wayland-server.h>
+#include <wayland-server-protocol.h>
+
 #include "compositor/wayland_compositor.h"
+
+/*
+ *
+ * Forward declarations
+ *
+ */
+
+/**
+ * Create a surface
+ */
+void
+create_surface(
+    struct wl_client* client, //!< client requesting the surface
+    struct wl_resource* resource, //!< the resource of the compositor
+    uint32_t serial //!< the serial to apply to the surface
+);
+
+/**
+ * Create a region
+ */
+void
+create_region(
+    struct wl_client* client, //!< the client requesting the region
+    struct wl_resource* resource, //!< the resource of the compositor
+    uint32_t serial //!< the serial to apply to the region
+);
+
+/*
+ *
+ * Internal constants
+ *
+ */
+
+/**
+ * Surface interface definition
+ *
+ * This interface definition holds all the methods for the surface type.
+ */
+static struct wl_compositor_interface interface = {
+    .create_surface = create_surface,
+    .create_region  = create_region,
+};
+
+/*
+ *
+ * Internal implementation
+ *
+ */
+
+void
+create_surface(
+    struct wl_client* client,
+    struct wl_resource* resource,
+    uint32_t serial
+) {
+    //!< @todo: implement
+}
+
+void
+create_region(
+    struct wl_client* client,
+    struct wl_resource* resource,
+    uint32_t serial
+) {
+    //!< @todo: implement
+}
 
