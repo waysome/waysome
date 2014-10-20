ad9010056535f6e16acc056b98d543fb13d1feb8
Julian Ganz <Julian.Ganz@hs-furtwangen.de>
2014-10-20 16:49:59 +0200

    Add error reply message type

---
+++ src/objects/message/error_reply.c
@@ -0,0 +1,52 @@
+/*
+ * waysome - wayland based window manager
+ *
+ * Copyright in alphabetical order:
+ *
+ * Copyright (C) 2014-2015 Julian Ganz
+ * Copyright (C) 2014-2015 Manuel Messner
+ * Copyright (C) 2014-2015 Marcel Müller
+ * Copyright (C) 2014-2015 Matthias Beyer
+ * Copyright (C) 2014-2015 Nadja Sommerfeld
+ *
+ * This file is part of waysome.
+ *
+ * waysome is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * waysome is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with waysome. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "objects/message/error_reply.h"
+
+
+/*
+ *
+ * Internal constants
+ *
+ */
+
+/**
+ * Type id of the `ws_error_reply` type.
+ */
+ws_object_type_id WS_OBJECT_TYPE_ID_ERROR_REPLY = {
+    .supertype  = &WS_OBJECT_TYPE_ID_REPLY,
+    .typestr    = "ws_reply",
+
+    .hash_callback = NULL,
+    .init_callback = NULL,
+    .deinit_callback = NULL,
+    .dump_callback = NULL,
+    .run_callback = NULL,
+    .cmp_callback = NULL,
+    .uuid_callback = NULL,
+};
+
