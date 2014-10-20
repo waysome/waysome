9d1a06af85d31cfc93cf166f55976fa2b171f794
Julian Ganz <Julian.Ganz@hs-furtwangen.de>
2014-10-20 23:41:53 +0200

    Add initialisation and deinitialisation

--- src/command/processor.c
+++ src/command/processor.c
@@ -25,11 +25,29 @@
  * along with waysome. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <stddef.h>
+#include <malloc.h>
+#include <unistd.h>
 
 #include "command/processor.h"
 #include "connection/connector.h"
 #include "objects/object.h"
+#include "serialize/deserializer.h"
+#include "serialize/serializer.h"
+
+/*
+ *
+ * Forward declarations
+ *
+ */
+
+/**
+ * Deinitialize a command processor
+ */
+bool
+command_processor_deinit(
+    struct ws_object * obj
+);
+
 
 /*
  *
@@ -54,7 +72,7 @@ ws_object_type_id WS_OBJECT_TYPE_ID_COMMAND_PROCESSOR = {
 
     .hash_callback = NULL,
     .init_callback = NULL,
-    .deinit_callback = NULL,
+    .deinit_callback = command_processor_deinit,
     .dump_callback = NULL,
     .run_callback = NULL,
     .cmp_callback = NULL,
@@ -73,7 +91,84 @@ ws_command_processor_new(
     struct ws_deserializer* deserializer, //!< deserializer to use
     struct ws_serializer* serializer //!< serializer to use
 ) {
-    //!< @todo implement
+    // check whether the fd is sane
+    if (fd < 0) {
+        return NULL;
+    }
+
+    // allocate memory for the ws_command_processor
+    struct ws_command_processor* retval = calloc(1, sizeof(*retval));
+    if (!retval) {
+        return NULL;
+    }
+
+    // initialize the underlying object
+    if (!ws_object_init(&retval->obj)) {
+        goto cleanup_mem;
+    }
+    retval->obj.id = &WS_OBJECT_TYPE_ID_COMMAND_PROCESSOR;
+    retval->obj.settings |= WS_OBJECT_HEAPALLOCED;
+
+    int res;
+
+    // check whether we have a readonly connection
+    if (serializer) {
+        // no
+        res = ws_connector_init(&retval->conn, fd);
+    } else {
+        // yes
+        res = ws_connector_init_readonly(&retval->conn, fd);
+    }
+    if (res < 0) {
+        goto cleanup_mem;
+    }
+
+    // initialize the (de)serializer
+    retval->deserializer    = deserializer;
+    retval->serializer      = serializer;
+
+    //!< @todo initialize and start a handler
+
+    // mark the object as initialized
+    retval->is_init = true;
+
+    // return the command processor
+    return retval;
+
+    // cleanups
+
+cleanup_mem:
+    ws_object_deinit(&retval->obj);
+    free(retval);
+
     return NULL;
+}
+
+
+/*
+ *
+ * Internal implementation
+ *
+ */
+
+bool
+command_processor_deinit(
+    struct ws_object * obj
+) {
+    struct ws_command_processor* proc = (struct ws_command_processor*) obj;
+
+    if (!proc->is_init) {
+        return true;
+    }
+
+    proc->is_init = false;
+
+    ws_connector_deinit(&proc->conn);
+
+    ws_deserializer_deinit(proc->deserializer);
+    if (proc->serializer) {
+        ws_serializer_deinit(proc->serializer);
+    }
+    return true;
 }
 
