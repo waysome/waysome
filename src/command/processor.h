6248d9932eddf9448e6650714aa3abb140d78bcb
Julian Ganz <Julian.Ganz@hs-furtwangen.de>
2014-10-20 21:30:40 +0200

    Add initial command processor interface and stubs

---
+++ src/command/processor.h
@@ -0,0 +1,68 @@
+/*
+ * waysome - wayland based window manager
+ *
+ * Copyright in alphabetical order:
+ *
+ * Copyright (C) 2014-2015 Julian Ganz
+ * Copyright (C) 2014-2015 Manuel Messner
+ * Copyright (C) 2014-2015 Marcel Müller
+ * Copyright (C) 2014-2015 Matthias Beyer
+ * Copyright (C) 2014-2015 Nadja Sommerfeld
+ *
+ * This file is part of waysome.
+ *
+ * waysome is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * waysome is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with waysome. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __WS_COMMAND_PROCESSOR_H__
+#define __WS_COMMAND_PROCESSOR_H__
+
+#include "objects/object.h"
+#include "util/attributes.h"
+
+// forward declarations
+struct ws_deserializer;
+struct ws_serializer;
+
+/**
+ * The command processor, which is an object
+ */
+struct ws_command_processor;
+
+/**
+ * Variable which holds type information about the ws_command_processor type
+ */
+extern ws_object_type_id WS_OBJECT_TYPE_ID_COMMAND_PROCESSOR;
+
+/**
+ * Create a command processor for a connection (which it creates internally)
+ *
+ * This function creates and starts a command processor for a given connection.
+ * For deserialisation, a `deserializer` must be supplied.
+ * However, the `serializer` is completely optional.
+ * If _no_ serializer is passed, the connection will be a read-only connection.
+ *
+ * @return a new command processor or `NULL`, if an error occured
+ */
+struct ws_command_processor*
+ws_command_processor_new(
+    int fd, //!< file descriptor to use run the command processor on
+    struct ws_deserializer* deserializer, //!< deserializer to use
+    struct ws_serializer* serializer //!< serializer to use
+)
+__ws_nonnull__(2)
+;
+
+#endif // __WS_COMMAND_PROCESSOR_H__
+
