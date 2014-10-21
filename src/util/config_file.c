c76283d30baa7162b727e77a66006df2183c3c6d
Julian Ganz <Julian.Ganz@hs-furtwangen.de>
2014-10-21 17:11:01 +0200

    Add function for testing variants

--- src/util/config_file.c
+++ src/util/config_file.c
@@ -25,7 +25,33 @@
  * along with waysome. If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <errno.h>
+#include <fcntl.h>
+#include <stdbool.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include "util/arithmetical.h"
 #include "util/config_file.h"
+
+/*
+ *
+ * Forward declarations
+ *
+ */
+
+/**
+ * Try all waysome configuration basenames
+ *
+ * @return a non-negative fd or a negative error number
+ */
+static int
+tryopen_variants(
+    char const* directory //!< Directory to search in
+);
+
 
 /*
  *
@@ -39,4 +65,54 @@ ws_config_load(void) {
     return -1;
 }
 
+
+/*
+ *
+ * Internal implementations
+ *
+ */
+
+static int
+tryopen_variants(
+    char const* directory
+) {
+    static char const* variants[] = {
+        "/waysome.conf",
+        "/waysome.json",
+        "/.waysome.conf",
+        "/.waysome.json",
+        "/waysome/waysome.conf",
+        "/waysome/waysome.json",
+    };
+
+    struct stat statbuf;
+    if ((stat(directory, &statbuf) != 0) || !S_ISDIR(statbuf.st_mode)) {
+        return -1;
+    }
+
+    const size_t dirlen = strlen(directory);
+
+    // iterate over all the variants
+    char const** var = variants + ARYLEN(variants);
+    while (var-- > variants) {
+        // assemble the path
+        char path[dirlen + strlen(*var) + 1];
+        (void) strcpy(path, directory);
+        (void) strcpy(path + dirlen, *var);
+
+        // try to open the file
+        int fd = open(path, O_RDONLY);
+        if (fd < 0) {
+            continue;
+        }
+
+        if ((fstat(fd, &statbuf) == 0) && S_ISREG(statbuf.st_mode)) {
+            return fd;
+        }
+
+        close(fd);
+    }
+
+    return -ENOENT;
+}
 
