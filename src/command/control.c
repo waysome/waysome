c9cd166c3dd877fb297aaed8201a8c0ce07c868d
Julian Ganz <Julian.Ganz@hs-furtwangen.de>
2014-10-22 16:05:31 +0200

    Add implementation of jump command

--- src/command/control.c
+++ src/command/control.c
@@ -25,14 +25,60 @@
  * along with waysome. If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <errno.h>
+
+#include "action/processor.h"
+#include "action/processor_stack.h"
 #include "command/control.h"
+#include "values/int.h"
+
 
 int
 ws_builtin_cmd_jump(
     struct ws_processor* proc,
     struct ws_command_args const* const args
 ) {
-    //!< @todo implement
-    return 0;
+    // the jump value is either embedded in an arg or this number itself
+    size_t value = args->num;
+
+    if (!value) {
+        // no (valid) arguments?
+        return -1;
+    }
+
+    struct ws_argument* arg = args->vals;
+    if (arg) {
+        // the value to jump is given by some ws_value
+        if (value != 1) {
+            // wrong number of arguments
+            return -EINVAL;
+        }
+
+        // get the value
+        struct ws_value* val;
+        switch(arg->type) {
+        case indirect: // the value must be extracted from the stack
+            val = ws_processor_stack_value_at(proc->stack, arg->arg.pos);
+            break;
+
+        case direct: // the value is passed directly
+            val = arg->arg.val;
+            break;
+
+        default:
+            return -EINVAL;
+        }
+
+        // check whether it's an int
+        if (val->type != WS_VALUE_TYPE_INT) {
+            // wrong type
+            return -EINVAL;
+        }
+
+        // set the jump value
+        value = ws_value_int_get((struct ws_value_int*) val);
+    }
+
+    return ws_processor_jump(proc, value);
 }
 
