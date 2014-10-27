5fc6102859cdd694382f9474cbea9c2275624c49
Julian Ganz <Julian.Ganz@hs-furtwangen.de>
2014-10-27 13:53:55 +0100

    Remove malicious assignment

--- src/input/input_device.c
+++ src/input/input_device.c
@@ -75,7 +75,6 @@ ws_object_type_id WS_OBJECT_TYPE_ID_INPUT_DEVICE = {
     .typestr    = "ws_input_device",
 
     .hash_callback = hash_callback,
-    .init_callback = NULL,
     .deinit_callback = NULL,
     .dump_callback = NULL,
     .run_callback = NULL,
