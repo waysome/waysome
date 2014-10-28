e86619c10eddf15bcb441d16d5b1f0507fff65a4
Matthias Beyer <mail@beyermatthias.de>
2014-10-28 15:13:24 +0100

    Add implementation of ws_builtin_cmd_bnot()

--- src/command/binary.c
+++ src/command/binary.c
@@ -25,14 +25,49 @@
  * along with waysome. If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <errno.h>
+
 #include "command/binary.h"
+#include "values/value_type.h"
+#include "values/union.h"
+
+/*
+ *
+ * static function prototypes
+ *
+ */
+
+/**
+ * Do a binary not on the passed argument
+ *
+ * @warning This is a helper function for the ws_builtin_cmd_bnot() function
+ * and should only be called after the argument array is validated. It doesn't
+ * do any validation
+ *
+ * @return zero on success, else negative errno.h number
+ */
+int
+bnot(
+    union ws_value_union* args
+);
+
+/*
+ *
+ * interface implementation
+ *
+ */
 
 int
 ws_builtin_cmd_bnot(
     union ws_value_union* args
 ) {
-    //!< @todo implement
-    return 0;
+    if (ws_value_get_type(&args->value) == WS_VALUE_TYPE_NONE ||
+            ws_value_get_type(&args[1].value) != WS_VALUE_TYPE_NONE) {
+        // There is no argument or there are more arguments than one
+        return -EINVAL;
+    }
+
+    return bnot(args);
 }
 
 int
@@ -81,3 +116,24 @@ ws_builtin_cmd_bxor(
     return 0;
 }
 
+/*
+ *
+ * static function implementations
+ *
+ */
+
+int
+bnot(
+    union ws_value_union* args
+) {
+    if (ws_value_get_type(&args->value) == WS_VALUE_TYPE_INT) {
+        int buf = ws_value_int_get(&args->int_);
+        ws_value_deinit(&args->value);
+        ws_value_int_init(&args->int_);
+        ws_value_int_set(&args->int_, ~buf);
+    } else {
+        return -EINVAL;
+    }
+
+    return 0;
+}
