de406cc922cd17fb19125f9b7960f5d6f0962036
Julian Ganz <Julian.Ganz@hs-furtwangen.de>
2014-10-30 00:22:16 +0100

    Add interface definitions and stubs for DAG

--- src/input/hotkey_dag.c
+++ src/input/hotkey_dag.c
@@ -25,5 +25,55 @@
  * along with waysome. If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <errno.h>
+#include <malloc.h>
+#include <string.h>
+
 #include "input/hotkey_dag.h"
+
+
+#define DAG_TAB_CHILD_NUM_EXP (4)
+#define DAG_TAB_CHILD_NUM (1 << DAG_TAB_CHILD_NUM_EXP)
+
+
+/*
+ *
+ * Interface implementation
+ *
+ */
+
+int
+ws_hotkey_dag_init(
+    struct ws_hotkey_dag_node* entry_node
+) {
+    // it's really as simple as...
+    memset(entry_node, 0, sizeof(*entry_node));
+    return 0;
+}
+
+void
+ws_hotkey_dag_deinit(
+    struct ws_hotkey_dag_node* entry_node
+) {
+    //!< @todo implement (now _this_ will get complicated...)
+}
+
+int
+ws_hotkey_dag_insert(
+    struct ws_hotkey_dag_node* node,
+    struct ws_hotkey_event* event
+) {
+    //!< @todo iterate over all the permutations possible, inserting
+    return -1;
+}
+
+int
+ws_hotkey_dag_remove(
+    struct ws_hotkey_dag_node* node,
+    struct ws_hotkey_event* event
+) {
+    //!< @todo remove an event, then all nodes linking to it, ...
+    return -1;
+}
+
 
--- src/input/hotkey_dag.h
+++ src/input/hotkey_dag.h
@@ -28,12 +28,95 @@
 #ifndef __WS_INPUT_HOTKEY_DAG_H__
 #define __WS_INPUT_HOTKEY_DAG_H__
 
+#include <stdint.h>
+
+#include "util/attributes.h"
+
+// forward declarations
+struct ws_hotkey_dag_tab_node;
+struct ws_hotkey_event;
+
+
+/**
+ * DAG node table
+ *
+ * This is a data type which gives the "table nodes" structure.
+ * It tells you two things: the `depth` of the tree and the start of the range
+ * covered by the table.
+ * See @ref hotkeys for more information.
+ */
+struct ws_hotkey_dag_tab {
+    union {
+        struct ws_hotkey_dag_node** dag;
+        void** tab;
+    } nodes; //!< @protected nodes in this tab
+    uint16_t start; //!< @protected stating point in the code space
+    uint8_t depth; //!< @protected indicator on how far we are from dag nodes
+};
+
 /**
  * Hotkey DAG node type
  *
+ * See @ref hotkeys for more information.
+ *
  * @note Pointers of this type are used as handles to carry state
  */
-struct ws_hotkey_dag_node;
+struct ws_hotkey_dag_node {
+    struct ws_hotkey_event* event; //!< @protected event stored in this node
+    struct ws_hotkey_dag_tab table; //!< @protected table containing more nodes
+};
+
+
+/**
+ * Initialize a new hotkey DAG
+ *
+ * This function initializes a DAG
+ *
+ * @return 0 if the initialization was successful, a negative error number
+ *         otherwise
+ */
+int
+ws_hotkey_dag_init(
+    struct ws_hotkey_dag_node* entry_node
+)
+__ws_nonnull__(1)
+;
+
+/**
+ * Destruct a DAG
+ */
+void
+ws_hotkey_dag_deinit(
+    struct ws_hotkey_dag_node* node
+)
+__ws_nonnull__(1)
+;
+
+/**
+ * Insert an event in a DAG
+ *
+ * @return 0 if the operation was successful, a negative error number otherwise
+ */
+int
+ws_hotkey_dag_insert(
+    struct ws_hotkey_dag_node* entry_node,
+    struct ws_hotkey_event* event
+)
+__ws_nonnull__(1, 2)
+;
+
+/**
+ * Remove an event from a DAG
+ *
+ * @return 0 if the operation was successful, a negative error number otherwise
+ */
+int
+ws_hotkey_dag_remove(
+    struct ws_hotkey_dag_node* node,
+    struct ws_hotkey_event* event
+)
+__ws_nonnull__(1, 2)
+;
 
 #endif // __WS_INPUT_HOTKEY_DAG_H__
 
