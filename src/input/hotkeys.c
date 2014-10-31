fea652d6c0c5f1f334512b6e41c575a7616d0e80
Julian Ganz <Julian.Ganz@hs-furtwangen.de>
2014-10-31 17:55:46 +0100

    Add event handling function

--- src/input/hotkeys.c
+++ src/input/hotkeys.c
@@ -30,8 +30,11 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-#include "input/hotkeys.h"
+#include "action/manager.h"
 #include "input/hotkey_dag.h"
+#include "input/hotkey_event.h"
+#include "input/hotkeys.h"
+#include "objects/message/event.h"
 #include "util/cleaner.h"
 
 /**
@@ -57,6 +60,20 @@ static void
 hotkeys_deinit(
     void* dummy
 );
+
+/**
+ * Reset the event list
+ *
+ * @return the old event list
+ */
+static struct wl_array
+eventlist_reset(void);
+
+/**
+ * Return an empty event list
+ */
+static struct wl_array
+empty_eventlist(void);
 
 /*
  *
@@ -93,10 +110,66 @@ struct wl_array
 ws_hotkeys_eval(
     struct input_event* ev
 ) {
-    //!< @todo implement
-    struct wl_array buf;
-    wl_array_init(&buf);
-    return buf;
+    // get the key code
+    uint16_t code = ev->code;
+
+    // insert the event into the track list
+    {
+        struct input_event* buf;
+        buf = wl_array_add(&ws_hotkeys_ctx.events, sizeof(*ev));
+        if (!buf) {
+            return eventlist_reset();
+        }
+        *buf = *ev;
+    }
+
+    // check whether the key was pressed or released
+    if (ev->value) {
+        ++ws_hotkeys_ctx.buttons_pressed;
+
+        // jump to the next DAG node
+        ws_hotkeys_ctx.state = ws_hotkey_dag_next(ws_hotkeys_ctx.state, code);
+        if (!ws_hotkeys_ctx.state) {
+            // this key is not part of a keycombo
+            return eventlist_reset();
+        }
+
+        return empty_eventlist();
+    }
+
+    // check whether we just removed the last key
+    --ws_hotkeys_ctx.buttons_pressed;
+    if (ws_hotkeys_ctx.buttons_pressed > 0) {
+        // nope. Means the input still might be a key combo
+        return empty_eventlist();
+    }
+
+    // we have something which may be a key combo. Let's find out what to do
+    if (!ws_hotkeys_ctx.state->event) {
+        // nothing!
+        return eventlist_reset();
+    }
+
+    // construct the event to emit
+    struct ws_event* event;
+    event = ws_event_new(&ws_hotkeys_ctx.state->event->name, NULL);
+    if (!event) {
+        return eventlist_reset();
+    }
+
+    // emit the event
+    struct ws_reply* reply;
+    reply = ws_action_manager_process((struct ws_message*) event);
+    if (reply) {
+        ws_object_unref((struct ws_object*) reply);
+    }
+
+    // reset the eventlist
+    wl_array_release(&ws_hotkeys_ctx.events);
+    wl_array_init(&ws_hotkeys_ctx.events);
+    ws_hotkeys_ctx.buttons_pressed = 0;
+
+    return ws_hotkeys_ctx.events;
 }
 
 /*
@@ -111,5 +184,20 @@ hotkeys_deinit(
 ) {
     wl_array_release(&ws_hotkeys_ctx.events);
     ws_hotkey_dag_deinit(&ws_hotkeys_ctx.root);
+}
+
+static struct wl_array
+eventlist_reset(void) {
+    struct wl_array buf = ws_hotkeys_ctx.events;
+    ws_hotkeys_ctx.buttons_pressed = 0;
+    wl_array_init(&ws_hotkeys_ctx.events);
+    return buf;
+}
+
+static struct wl_array
+empty_eventlist(void) {
+    struct wl_array buf;
+    wl_array_init(&buf);
+    return buf;
 }
 
