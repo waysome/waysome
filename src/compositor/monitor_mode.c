7d2e87ec3495b69ebfb95e10a3287aeb0aa325e3
Matthias Beyer <mail@beyermatthias.de>
2014-11-04 16:10:19 +0100

    Add missing member NULLing for ws_monitor_mode type

--- src/compositor/monitor_mode.c
+++ src/compositor/monitor_mode.c
@@ -59,7 +59,9 @@ ws_object_type_id WS_OBJECT_TYPE_ID_MONITOR_MODE = {
     .hash_callback = monitor_mode_hash,
     .dump_callback = NULL,
     .run_callback = NULL,
-    .cmp_callback = monitor_mode_cmp
+    .cmp_callback = monitor_mode_cmp,
+
+    .attribute_table = NULL,
 };
 
 /*
