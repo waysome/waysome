16cf58e079ce94dfd8d1ef971ce829a98c733d2d
Matthias Beyer <mail@beyermatthias.de>
2014-11-09 14:10:00 +0100

    Add test for tree traversing

--- tests/check/input/input_tests.c
+++ tests/check/input/input_tests.c
@@ -37,18 +37,109 @@
  * @{
  */
 
+#include <stdint.h>
+#include <stdlib.h>
+
 #include <check.h>
 
 #include "tests.h"
 #include "input/hotkey_dag.h"
+#include "input/hotkey_event.h"
+#include "objects/string.h"
+#include "util/arithmetical.h"
 
 #include <stdlib.h>
+
+static struct ws_string*
+mkstr(
+    char* lit
+) {
+    struct ws_string* s = ws_string_new();
+    ck_assert(s);
+    ws_string_set_from_raw(s, lit);
+    return s;
+}
 
 START_TEST (test_input_dag_get_node_from_empty_node) {
     struct ws_hotkey_dag_node* node = calloc(1, sizeof(*node));
     ck_assert(node);
     ck_assert(NULL == ws_hotkey_dag_next(node, 0));
     free(node);
+}
+END_TEST
+
+START_TEST (test_input_dag_insert_hotkey_events_into_dag) {
+    struct ws_hotkey_dag_node* node = calloc(1, sizeof(*node));
+    ck_assert(node);
+    ck_assert(0 == ws_hotkey_dag_init(node));
+
+    struct ws_string* tmpname = mkstr("hello");
+
+    uint16_t codes1[]   = { 1, 2, 15 }; // This is a key-combo
+    uint16_t ncodes1    = 3;
+    struct ws_hotkey_event* ev1 = ws_hotkey_event_new(tmpname, codes1, ncodes1);
+    ck_assert(ev1);
+
+    uint16_t codes2[]   = { 42, 22, 5 }; // This is a key-combo
+    uint16_t ncodes2    = 3;
+    struct ws_hotkey_event* ev2 = ws_hotkey_event_new(tmpname, codes2, ncodes2);
+    ck_assert(ev2);
+
+    uint16_t codes3[]   = { 42, 22, 6 }; // This is a key-combo
+    uint16_t ncodes3    = 3;
+    struct ws_hotkey_event* ev3 = ws_hotkey_event_new(tmpname, codes3, ncodes3);
+    ck_assert(ev3);
+
+    ck_assert(0 == ws_hotkey_dag_insert(node, ev1));
+    ck_assert(0 == ws_hotkey_dag_insert(node, ev2));
+    ck_assert(0 == ws_hotkey_dag_insert(node, ev3));
+
+    // try to run through the tree with the following key codes
+    struct ws_hotkey_dag_node* nxt = NULL;
+
+    uint16_t trav_codes_1_succ[] = { 1,    2,  15 };
+    nxt = ws_hotkey_dag_next(node,  trav_codes_1_succ[0]);
+    ck_assert(nxt != NULL);
+    nxt = ws_hotkey_dag_next(nxt,   trav_codes_1_succ[1]);
+    ck_assert(nxt != NULL);
+    nxt = ws_hotkey_dag_next(nxt,   trav_codes_1_succ[2]);
+    ck_assert(nxt != NULL);
+
+    uint16_t trav_codes_2_succ[] = { 42,   22, 5 };
+    nxt = ws_hotkey_dag_next(node,  trav_codes_2_succ[0]);
+    ck_assert(nxt != NULL);
+    nxt = ws_hotkey_dag_next(nxt,   trav_codes_2_succ[1]);
+    ck_assert(nxt != NULL);
+    nxt = ws_hotkey_dag_next(nxt,   trav_codes_2_succ[2]);
+    ck_assert(nxt != NULL);
+
+    uint16_t trav_codes_3_succ[] = { 42,   22, 6 };
+    nxt = ws_hotkey_dag_next(node,  trav_codes_3_succ[0]);
+    ck_assert(nxt != NULL);
+    nxt = ws_hotkey_dag_next(nxt,   trav_codes_3_succ[1]);
+    ck_assert(nxt != NULL);
+    nxt = ws_hotkey_dag_next(nxt,   trav_codes_3_succ[2]);
+    ck_assert(nxt != NULL);
+
+    uint16_t trav_codes_4_fail[] = { 42,   22, 15 };
+    nxt = ws_hotkey_dag_next(node,  trav_codes_4_fail[0]);
+    ck_assert(nxt != NULL);
+    nxt = ws_hotkey_dag_next(nxt,   trav_codes_4_fail[1]);
+    ck_assert(nxt != NULL);
+    nxt = ws_hotkey_dag_next(nxt,   trav_codes_4_fail[2]);
+    ck_assert(nxt == NULL); // There is no keycode 42-22-15 combo
+
+    uint16_t trav_codes_5_fail[] = { 1,    3,  15 };
+    nxt = ws_hotkey_dag_next(node,  trav_codes_5_fail[0]);
+    ck_assert(nxt != NULL);
+    nxt = ws_hotkey_dag_next(nxt,   trav_codes_5_fail[1]);
+    ck_assert(nxt != NULL);
+    nxt = ws_hotkey_dag_next(nxt,   trav_codes_5_fail[2]);
+    ck_assert(nxt == NULL); // There is no keycode 1-3-15 combo
+
+    ws_object_unref((struct ws_object*) tmpname);
+    ws_object_unref((struct ws_object*) ev1);
+    ws_object_unref((struct ws_object*) ev2);
 }
 END_TEST
 
@@ -62,6 +153,7 @@ input_dag_suite(void)
     // tcase_add_checked_fixture(tc, setup, cleanup); // Not used yet
 
     tcase_add_test(tc, test_input_dag_get_node_from_empty_node);
+    tcase_add_test(tc, test_input_dag_insert_hotkey_events_into_dag);
 
     return s;
 }
