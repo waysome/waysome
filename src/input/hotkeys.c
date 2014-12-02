ba7de9667a9cb23e8693fffbcc5a259ea44612e2
Julian Ganz <Julian.Ganz@hs-furtwangen.de>
2014-12-02 13:57:29 +0100

    Fix recognicion of repeated events
    Turns out the value is not `1` for repeated events, but `2`. This means
    that:
     * my previous approach broke because I was comparing the value directly
     * instead of looking things up in an array, I can just look at the
       value and use _this_ as a base for recognicion of repeated events

--- src/input/hotkeys.c
+++ src/input/hotkeys.c
@@ -115,30 +115,18 @@ struct wl_array
 ws_hotkeys_eval(
     struct input_event* ev
 ) {
+    // ignore repeated or unknown events
+    if ((ev->value < 0) || (ev->value > 1)) {
+        return empty_eventlist();
+    }
+
     // get the key code
     uint16_t code = ev->code;
 
-    // insert the event into the track list, if it's not yet in the event list
+    // insert the event into the track list, if it's not a repeat event
     {
-        struct input_event* buf;
-
-        // check whether the event already is accounted for
-        struct input_event* const data = ws_hotkeys_ctx.events.data;
-        buf = data + ws_hotkeys_ctx.events.size;
-        while (buf-- > data) {
-            if (buf->code != code) {
-                // event not of interest
-                continue;
-            }
-            if (buf->value != ev->value) {
-                // the button was release before
-                break;
-            }
-            // we consume the event but we'll _not_ store it or iterate the DAG
-            return empty_eventlist();
-        }
-
         // now insert the event into the event list
+        struct input_event* buf;
         buf = wl_array_add(&ws_hotkeys_ctx.events, sizeof(*ev));
         if (!buf) {
             return eventlist_reset();
