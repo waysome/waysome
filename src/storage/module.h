1a3e5dcc47629e1707191ab1641a89be5eb1328b
Matthias Beyer <mail@beyermatthias.de>
2014-12-09 11:53:04 +0100

    Add storage files to storage documentation group

--- src/storage/module.h
+++ src/storage/module.h
@@ -25,9 +25,20 @@
  * along with waysome. If not, see <http://www.gnu.org/licenses/>.
  */
 
+/**
+ * @addtogroup storage "Storage"
+ *
+ * @{
+ */
+
 #ifndef __WS_STORAGE_MODULE_H__
 #define __WS_STORAGE_MODULE_H__
 
 
 
 #endif // __WS_STORAGE_MODULE_H__
+
+/**
+ * @}
+ */
+
