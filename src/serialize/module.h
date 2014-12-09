b7427a50438208d4399eb196092e7293358d9572
Matthias Beyer <mail@beyermatthias.de>
2014-12-09 11:47:27 +0100

    Add all abstract serializer stuff to serializer doc group

--- src/serialize/module.h
+++ src/serialize/module.h
@@ -25,9 +25,20 @@
  * along with waysome. If not, see <http://www.gnu.org/licenses/>.
  */
 
+/**
+ * @addtogroup serializer "Serializer"
+ *
+ * @{
+ */
+
 #ifndef __WS_SERIALIZE_MODULE_H__
 #define __WS_SERIALIZE_MODULE_H__
 
 
 
 #endif // __WS_SERIALIZE_MODULE_H__
+
+/**
+ * @}
+ */
+
--- src/serialize/serializer.h
+++ src/serialize/serializer.h
@@ -25,6 +25,18 @@
  * along with waysome. If not, see <http://www.gnu.org/licenses/>.
  */
 
+/**
+ * @addtogroup serializer "Serializer"
+ *
+ * @{
+ */
+
+/**
+ * @addtogroup serializer_deserializer "Serializer: Serializer"
+ *
+ * @{
+ */
+
 #ifndef __WS_SERIALIZE_SERIALIZER_H__
 #define __WS_SERIALIZE_SERIALIZER_H__
 
@@ -112,4 +124,12 @@ __ws_nonnull__(1)
 ;
 
 #endif //__WS_SERIALIZE_SERIALIZER_H__
+
+/**
+ * @}
+ */
+
+/**
+ * @}
+ */
 
